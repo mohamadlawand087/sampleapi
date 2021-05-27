72bf527b554df628a90ed27bee59b586d3934d1f
mohamad <mohamad.lawand@outlook.com>
2021-05-27 07:52:48 +0100

    updates on tf providers

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 # we need to specify the provider that we are going to use
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  required_providers = "2.5.0"
    features {
     
   }
